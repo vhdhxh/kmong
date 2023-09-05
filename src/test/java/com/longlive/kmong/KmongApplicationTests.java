61e99eff1a617ea235098cf151340132ecd19eb4
dev yoon <vhdhxh@naver.com>
2023-09-06 00:20:45 +0900

    부트스트랩 헤더메인추가,타임리프 레이아웃 나누기

--- src/test/java/com/longlive/kmong/KmongApplicationTests.java
+++ src/test/java/com/longlive/kmong/KmongApplicationTests.java
@@ -2,8 +2,10 @@ package com.longlive.kmong;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootTest
+@WebAppConfiguration
 class KmongApplicationTests {
 
 	@Test
