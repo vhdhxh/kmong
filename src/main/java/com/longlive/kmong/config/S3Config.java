157517ace0d10626b1e732c127572d42a42bb3d5
dev yoon <vhdhxh@naver.com>
2023-11-21 01:11:36 +0900

    s3 업로드 리팩토링

--- src/main/java/com/longlive/kmong/config/S3Config.java
+++ src/main/java/com/longlive/kmong/config/S3Config.java
@@ -34,6 +34,7 @@ public class S3Config {
     public AmazonS3Client amazonS3Client() {
         BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
+
         return (AmazonS3Client) AmazonS3ClientBuilder
                 .standard()
                 .withRegion(region)
