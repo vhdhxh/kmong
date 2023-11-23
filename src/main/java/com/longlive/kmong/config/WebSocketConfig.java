4ed4589b34231155016d78844fd5eac936a65da9
dev yoon <vhdhxh@naver.com>
2023-11-23 12:51:24 +0900

    sockjs CORS 설정

--- src/main/java/com/longlive/kmong/config/WebSocketConfig.java
+++ src/main/java/com/longlive/kmong/config/WebSocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @RequiredArgsConstructor
@@ -28,6 +29,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat/stomp-chatting") //SockJS 연결주소
+                .addInterceptors(new HttpSessionHandshakeInterceptor())
+                .setAllowedOrigins("*")
                 .withSockJS();
         // 주ㅡ소 : ws://localhost:8080/ws-stomp
     }
